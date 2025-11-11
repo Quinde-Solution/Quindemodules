5d24dc6d3dcfb079fd27b420f9d481a1c4258d80
Quinde-Solution <directorgeneral@quindesolution.com>
2025-11-11 09:20:40 -0500

    Add files via upload

---
+++ portalweb_gr/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Portal GR',
+    'version': '1.0',
+    'summary': 'Portal para gestión de facturas XML contra órdenes de compra',
+    'description': 'Permite a proveedores subir XMLs y validarlos contra OC',
+    'category': 'Portal',
+    'author': 'Ricardo Aucapiña',
+    'license': 'LGPL-3',
+    'depends': ['purchase', 'portal', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/gr_xml_upload_views.xml',
+        'views/purchase_order_inherit.xml',
+        'views/portal_gr_template.xml',
+        'views/portal_purchase_inherit.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ portalweb_gr/models/__init__.py
@@ -0,0 +1,2 @@
+from . import purchase_order
+from . import gr_xml_upload
